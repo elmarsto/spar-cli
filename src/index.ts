4ff12b3ac77abd95d3635576e6a513342373f326
Elizabeth Marston <elmarsto@microsoft.com>
2020-12-20 19:07:00 -0800

    progress

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ const clear = require('clear');
 const figlet = require('figlet');
 const path = require('path');
 const program = require('commander');
-const spar  = require("spar-lib");
+const spar  = require('spar-lib');
 
 console.log(spar.translate("some other str"));
